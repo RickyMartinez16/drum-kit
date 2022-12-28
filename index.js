923aa11db620311161e657bbf67fea47f2932a0a
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-28 17:58:32 -0500

    snare

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 //audio function
 function handleClick(){
-    let audio = new Audio("sounds/kick-bass.mp3");
+    // let audio = new Audio("sounds/kick-bass.mp3");
     audio.play()
 }
 
 for(let i = 0; i < document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", handleClick)
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
 
-    let buttonInnerHTML = this.innerHTML;
+        let buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
-        case "w":
-        let audio = new Audio("sounds/crash.mp3");
-        audio.play()
-            break;
-    
-        default:
-            break;
-    }
+        switch (buttonInnerHTML) {
+            case "w":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play()
+                break;
+
+            case "a":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play()
+                break;
+
+            case "s":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play()
+                break;
+
+            
+        
+            default:
+                break;
+        }
+
+    })
+
 }
 
