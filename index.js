37d126a4b34d8a6c70c3478a133e69af6660166d
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-28 18:02:08 -0500

    toms

--- index.js
+++ index.js
@@ -25,8 +25,27 @@ for(let i = 0; i < document.querySelectorAll(".drum").length; i++){
             snare.play()
                 break;
 
-            
+            case "d":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play()
+                break;
+
+            case "j":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play()
+                break;
         
+            case "k":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play()
+                break;
+
+            case "l":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play()
+                break;
+            
+
             default:
                 break;
         }
